87e5d618d240eae95e94ce428785dd211829c235
Muky-dev <ti.gabrielfini@gmail.com>
2021-06-01 13:41:28 -0300

    Removed JWT

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import expressSession from 'express-session';
-import passport from 'passport';
-import Auth0Strategy from 'passport-auth0';
 import multer from 'multer';
 import expressLayouts from 'express-ejs-layouts';
+
 if (process.env.NODE_ENV !== 'production') { dotenv.config(); }
 
 // APP
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// EJS
 
 app.set('view engine', 'ejs');
 app.set('views', process.cwd() + '/views');
@@ -30,33 +31,7 @@ app.use(expressSession({
 // If HTTPS use secure cookies
 if (process.env.NODE_ENV === 'production') session.cookie.secure = true;
 
-// Auth0 configs
-const strategy = new Auth0Strategy(
-    {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        callbackURL: process.env.AUTH0_CALLBACK_URL
-    },
-
-    function (accessToken, refreshToken, extraParams, profile, done) {
-        return done(null, profile);
-    }
-);
-
 // PASSPORT
-passport.use(strategy);
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-    done(null, user);
-});
-
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.isAuthenticated();
     next();
