d48f8992081c534970ac11157650186c63357c79
Muky-dev <ti.gabrielfini@gmail.com>
2021-06-01 13:43:20 -0300

    Project main controller

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,50 @@
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import User from "../models/User.js";
+import Post from "../models/Post.js";
+import cfg from '../conf/jwtconfig.js'
+
 export async function getFeed(req, res) {
-    res.send('Working page');
+    let posts
+    try {
+        posts = await Post.find().exec();
+    } catch {
+        posts = []
+    }
+    res.render('index', { posts: posts });
+}
+
+export async function newUser(req, res) {
+    const { username, email, nickname, password } = req.body;
+    const localStrategy = new passportLocal.Strategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    });
+    passport.use('signup', localStrategy, async (email, password, done) => {
+        
+    });
+    try {
+        const newUser = await new User(
+            {
+                username: username,
+                email: email,
+                nickname: nickname,
+                password: password
+            }).save();
+        res.json(newUser);
+    } catch (error) {
+        res.json(error);
+    }
+}
+
+export async function generateToken(req, res) {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ email: email, password: password });
+        const payload = { id: user._id };
+        const token = jwt.encode(payload, cfg.jwtSecret);
+        res.json({ token: token });
+    } catch (error) {
+        res.json(error);
+    }
 }
