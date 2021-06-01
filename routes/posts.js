ecee310fa78882a4c993f4502bb6e8882cd9d285
Muky-dev <ti.gabrielfini@gmail.com>
2021-06-01 13:46:58 -0300

    Post main routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,31 @@
 import express from 'express';
 import * as controllerPosts from '../controller/posts.js';
+import Post from "../models/Post.js";
+
 const router = express.Router();
 
-router.get('/', controllerPosts.getPosts);
 router.get('/new', controllerPosts.getNewPostPage);
+
+router.get('/:id', controllerPosts.getSinglePost);
 router.post('/', controllerPosts.postPosts);
+router.patch('/:id', getPostId, controllerPosts.patchPosts);
+router.delete('/:id', getPostId, controllerPosts.deletePosts);
+
+async function getPostId (req, res, next) {
+
+	let post;
+
+	try {
+		post = await Post.findById(req.params.id);
+		if (post == null) {console.log("post null")};
+	} catch (error) {
+		console.log(error);
+	}
+
+	res.post = post;
+
+	next();
+
+}
 
 export default router;
