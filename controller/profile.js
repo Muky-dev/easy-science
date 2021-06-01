ae5858d0499ca9d4e86f4893b3be3be4c47b2ff8
Muky-dev <ti.gabrielfini@gmail.com>
2021-06-01 13:44:24 -0300

    Profile controller return

--- controller/profile.js
+++ controller/profile.js
@@ -1,3 +1,14 @@
-export async function getProfile (req, res) {
+import User from "../models/User.js";
 
+
+
+export async function getProfile(req, res) {
+    try {
+        const user = await User.findOne({ _id: req.user.id });
+        res.json({ user: user });
+    } catch (error) {
+        res.json(error);
+    }
 }
+
+
