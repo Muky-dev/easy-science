4d0bd9802d27a7811366d085d2e6ffbcb7802b74
Muky-dev <ti.gabrielfini@gmail.com>
2021-06-01 13:46:00 -0300

    Removed auth

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as controllerIndex from '../controller/index.js';
-import * as auth from '../controller/auth.js';
 const router = express.Router();
 
 router.get('/', controllerIndex.getFeed);
 
 // AUTH
-router.get('/login', auth.login);
+router.post('/register', controllerIndex.newUser);
+router.post('/login', controllerIndex.generateToken);
+/*
 router.get('/logout', auth.logout);
-router.get('/callback', auth.callback);
+router.get('/callback', auth.callback);*/
 
 export default router;
